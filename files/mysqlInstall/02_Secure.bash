d4626258b8443462266468469b3b4c6c3d8d0dbb
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2019-01-14 12:11:12 +0000

    initial commit

---
+++ files/mysqlInstall/02_Secure.bash
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+cd $DIR;
+set -e
+set -u
+set -o pipefail
+standardIFS="$IFS"
+IFS=$'\n\t'
+echo "
+===========================================
+$(hostname) $0 $@
+===========================================
+"
+
+if [[ "$(whoami)" != "root" ]]
+then
+    echo "Please run this as root"
+    exit 1
+fi
+
+perconaGeneratedPass="$(cat /var/log/mysqld.log |grep generated | cut -d ':' -f 4 | xargs)"
+
+mysqlRootPassword=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32;echo;)
+
+
+echo "---------------------------------------"
+echo "MySQL root Password: $mysqlRootPassword"
+echo "---------------------------------------"
+
+echo "Securing MySQL Installation"
+mysql -p"${perconaGeneratedPass}" -e "
+UPDATE mysql.user SET Password=PASSWORD('$mysqlRootPassword') WHERE User='root';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
+FLUSH PRIVILEGES;
+"
+
+echo "Done"
+
+echo "Configuring .my.cnf Files"
+echo "
+[client]
+user=root
+password=$mysqlRootPassword
+"   > ~/.my.cnf
+chmod 600 ~/.my.cnf
+
+echo "
+[client]
+user=root
+password=$mysqlRootPassword
+"   > /home/ec/.my.cnf
+
+chown ec:ec /home/ec/.my.cnf
+chmod 600 /home/ec/.my.cnf
+echo "Done"
+
+echo "Installing mytop"
+yum -y install mytop
+echo "
+user=root
+pass=$mysqlRootPassword
+host=localhost
+db=information_schema
+delay=5
+port=3306
+batchmode=0
+header=1
+color=1
+idle=1
+" > ~/.mytop
+chmod 600 ~/.mytop
+echo "
+user=root
+pass=$mysqlRootPassword
+host=localhost
+db=information_schema
+delay=5
+port=3306
+batchmode=0
+header=1
+color=1
+idle=1
+" > /home/ec/.mytop
+chown ec:ec /home/ec/.mytop
+chmod 600 /home/ec/.mytop
+
+
+echo "
+----------------
+$(hostname) $0 completed
+----------------
+"
