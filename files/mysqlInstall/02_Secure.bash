019a2613327ca76022f4c8f99a6fd2ee6bedc06d
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2019-01-14 15:28:21 +0000

    work in progress

--- files/mysqlInstall/02_Secure.bash
+++ files/mysqlInstall/02_Secure.bash
@@ -20,22 +20,14 @@ fi
 
 perconaGeneratedPass="$(cat /var/log/mysqld.log |grep generated | cut -d ':' -f 4 | xargs)"
 
-mysqlRootPassword=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32;echo;)
+mysqlRootPassword="@$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32;echo;)"
 
 
 echo "---------------------------------------"
 echo "MySQL root Password: $mysqlRootPassword"
 echo "---------------------------------------"
 
-echo "Securing MySQL Installation"
-mysql -p"${perconaGeneratedPass}"  --connect-expired-password -e "
-UPDATE mysql.user SET Password=PASSWORD('$mysqlRootPassword') WHERE User='root';
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
-FLUSH PRIVILEGES;
-"
+mysqladmin -p"$perconaGeneratedPass" -u root password "$mysqlRootPassword"
 
 echo "Done"
 
@@ -47,15 +39,20 @@ password=$mysqlRootPassword
 "   > ~/.my.cnf
 chmod 600 ~/.my.cnf
 
+echo "Securing MySQL Installation"
+mysql  -e "
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
+FLUSH PRIVILEGES;
+"
+
 echo "
 [client]
 user=root
 password=$mysqlRootPassword
 "   > /home/ec/.my.cnf
-
-chown ec:ec /home/ec/.my.cnf
-chmod 600 /home/ec/.my.cnf
-echo "Done"
 
 echo "Installing mytop"
 yum -y install mytop
@@ -71,6 +68,13 @@ header=1
 color=1
 idle=1
 " > ~/.mytop
+
+
+chown ec:ec /home/ec/.my.cnf
+chmod 600 /home/ec/.my.cnf
+echo "Done"
+
+
 chmod 600 ~/.mytop
 echo "
 user=root
